5b3c4c0855a9fba236cd81ada54c09a60ffc701e
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-05-01 14:46:19 +0100

    don't re-download if file already exists

--- scripts/01b_get_data.r
+++ scripts/01b_get_data.r
@@ -1,4 +1,7 @@
-download.file(
-  "https://github.com/sbfnk/ebola.forecast.wa.sl/raw/master/data/ebola_wa.rdata",
-  here::here("data", "ebola_wa.rdata")
-)
+ebola_data_file <- here::here("data", "ebola_wa.rdata")
+if (!file.exists(ebola_data_file)) {
+  download.file(
+    "https://github.com/sbfnk/ebola.forecast.wa.sl/raw/master/data/ebola_wa.rdata",
+    ebola_data_file
+  )
+}
