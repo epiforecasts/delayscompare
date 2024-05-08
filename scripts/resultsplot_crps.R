3c820831aea7d9f978e14d2516956c052ff7e17e
Ciara McCarthy <Ciara.McCarthy1@lshtm.ac.uk>
2024-05-08 10:06:30 +0100

    Plot functions

--- scripts/resultsplot_crps.R
+++ scripts/resultsplot_crps.R
@@ -5,84 +5,22 @@ library(scoringutils)
 library(viridis)
 library(RColorBrewer)
 
+####################
+#### EBOLA-LIKE ####
+####################
+
 ## Loading results ##
 
-ebola_samples <- readRDS(here("results", paste0("res_ebola_samples", "2024-05-07", ".rds")))
+ebola_samples <- readRDS(here("results", paste0("res_ebola_samples", "2024-05-08", ".rds")))
 res_ebola_id <- readRDS(here("results", paste0("res_ebola2_id", "2024-04-24", ".rds")))
 ebola_sim_data <- readRDS(here("data", paste0("ebola_sim_data", "2024-04-23", ".rds")))
 
-## CRPS ##
+## CRPS plot ##
 
-# Add simulated data
-ebola_samples <- ebola_sim_data |> 
-  filter(variable=="reported_cases") |>
-  rename(true_value=value) |>
-  select(-variable) |>
-  right_join(ebola_samples, by="date") 
+plot_crps_ebola <- plotcrps(ebola_samples, 
+                            res_ebola_id, 
+                            ebola_sim_data)
 
-check_forecasts(ebola_samples)
-
-# Get rid of all columns that aren't date, true_value, prediction, sample
-
-ebola_samples <- ebola_samples |>
-  select(date, true_value, prediction, sample, model, result_list, type)
-
-# Log transform observations and predicted values
-
-ebola_samples <- transform_forecasts(ebola_samples, fun = log_shift, offset=1, label="log")
-
-ebola_samples |>
-  check_forecasts()
-
-ebola_scores <- ebola_samples |>
-  # filtering out what I don't need to save memory
-  filter(type=="forecast", scale=="log") |>
-  set_forecast_unit(c("date", "model", "result_list", "type")) |>
-  score() |>
-  summarise_scores(by=c("model", "type", "result_list"))
-
-## Add the info for each scenario to the plot
-ebola_scores_details <- ebola_scores |>
-  left_join(res_ebola_id, by="result_list")
-
-### Heatmap by timepoint ###
-
-# Need to turn inc_period and gen_time into factors to make sure ordering is correct
-ebola_scores_details$inc_period <- factor(ebola_scores_details$inc_period, levels=c("very low", "low", "correct", "high", "very high"))
-ebola_scores_details$gen_time <- factor(ebola_scores_details$gen_time, levels=c("very low", "low", "correct", "high", "very high"))
-
-ebola_sim_data_cases <- ebola_sim_data |> 
-  filter(variable=="reported_cases") 
-
-ebola_timepoints <- ebola_sim_data_infections$date[c(1:(nrow(ebola_sim_data_cases) %/% (8*7)))*8*7]
-
-ebola_sim_data_timepoints <- ebola_sim_data_infections |> filter(date %in% ebola_timepoints)
-
-timeseries_ebola <- ggplot() + 
-  geom_line(ebola_sim_data_cases, mapping=aes(x=date, y=value)) + 
-  geom_point(ebola_sim_data_timepoints, mapping=aes(x=date,y=value), color="red") +
-  xlab("Date") +
-  ylab("Reported cases") +
-  scale_x_continuous(breaks=ebola_sim_data_cases$date[grepl("-01$",ebola_sim_data_cases$date)]) +
-  lshtm_theme()
-
-timepoints_lab <- data.frame(timepoint=c(1:length(ebola_timepoints)),
-                             timepoint_lab=ebola_timepoints)
-
-ebola_scores_details <- ebola_scores_details |>
-  left_join(timepoints_lab, by="timepoint")
-
-heatmap_ebola <- 
-  ggplot(ebola_scores_details, aes(x=gen_time, y=inc_period)) +
-  geom_tile(aes(fill=crps)) +
-  facet_wrap(~timepoint_lab, nrow=1) +
-  xlab("Generation time") +
-  ylab("Incubation period") +
-  scale_fill_gradientn(colours = terrain.colors(50), name="CRPS for one-week forecast") +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle=45, hjust=1)) +
-  lshtm_theme()
-
-heatmap_ebola_space <- plot_grid(NULL, heatmap_ebola, NULL, rel_widths=c(0.08, 1, 0.05), nrow=1)
-
-plot_grid(timeseries_ebola, heatmap_ebola_space, ncol=1)
+plot_forecasts_ebola <- plotforecasts(ebola_samples,
+                                      res_ebola_id,
+                                      ebola_sim_data)
--- scripts/resultsplot_forecasts.R
+++ scripts/resultsplot_forecasts.R
@@ -1,30 +1,37 @@
 
-ebola_samples_plot <- ebola_samples |> 
-  filter(type=="forecast", scale=="natural") |>
+plotforecasts <- function(res_samples,
+                          res_id,
+                          sim_data){
+
+res_samples_plot <- res_samples |> 
+  filter(type=="forecast") |>
   # add info
-  left_join(res_ebola_id, by="result_list")
+  left_join(res_id, by="result_list")
+
+sim_data_cases <- sim_data |> 
+  filter(variable=="reported_cases") 
 
 # Add rest of data
-ebola_samples_plot <- ebola_sim_data_infections |>
+res_samples_plot <- sim_data_cases |>
   rename(true_value=value) |>
   select(date, true_value) |>
-  left_join(ebola_samples_plot |> select(-true_value), by="date")
+  left_join(res_samples_plot, by="date")
 
-ebola_samples_plot$gen_time <- factor(ebola_samples_plot$gen_time, levels=c("very low", "low", "correct", "high", "very high"))
-ebola_samples_plot$inc_period <- factor(ebola_samples_plot$inc_period, levels=c("very low", "low", "correct", "high", "very high"))
+res_samples_plot$gen_time <- factor(res_samples_plot$gen_time, levels=c("very low", "low", "correct", "high", "very high"))
+res_samples_plot$inc_period <- factor(res_samples_plot$inc_period, levels=c("very low", "low", "correct", "high", "very high"))
 
 #### Generation time plot ####
 
-ebola_samples_plot_gentime <- ebola_samples_plot |>
+res_samples_plot_gentime <- res_samples_plot |>
   filter(is.na(inc_period) | inc_period=="correct")
 
-ebola_samples_plot_gentime <- sample_to_quantile(ebola_samples_plot_gentime, quantiles = c(0.05, 0.25, 0.5, 0.75, 0.95), type = 7)
+res_samples_plot_gentime <- sample_to_quantile(res_samples_plot_gentime, quantiles = c(0.05, 0.25, 0.5, 0.75, 0.95), type = 7)
 
-ebola_samples_plot_gentime <- ebola_samples_plot_gentime |>
+res_samples_plot_gentime <- res_samples_plot_gentime |>
   pivot_wider(names_from="quantile",
               values_from="prediction")
 
-ebola_samples_plot_gentime <- ebola_samples_plot_gentime |>
+res_samples_plot_gentime <- res_samples_plot_gentime |>
   rename(lower90="0.05",
          upper90="0.95",
          lower50="0.25",
@@ -33,104 +40,75 @@ ebola_samples_plot_gentime <- ebola_samples_plot_gentime |>
 # filter(!is.na(gen_time))
 
 # Get rid of NA
-ebola_samples_plot_gentime <- ebola_samples_plot_gentime |>
+res_samples_plot_gentime <- res_samples_plot_gentime |>
   filter(!is.na(gen_time))
 
-## Need to find a better way to do this ##
-plot_gentime <- ggplot() +
-  geom_line(ebola_sim_data_infections |> filter(date<="2015-04-07"), mapping=aes(x=date, y=value)) +
-  geom_line(ebola_samples_plot_gentime |> filter(timepoint==1 | is.na(timepoint)), mapping=aes(x=date, y=median, color=gen_time)) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==1 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=gen_time), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==1 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=gen_time), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_gentime |> filter(timepoint==2 | is.na(timepoint)), mapping=aes(x=date, y=median, color=gen_time)) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==2 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=gen_time), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==2 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=gen_time), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_gentime |> filter(timepoint==3 | is.na(timepoint)), mapping=aes(x=date, y=median, color=gen_time)) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==3 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=gen_time), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==3 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=gen_time), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_gentime |> filter(timepoint==4 | is.na(timepoint)), mapping=aes(x=date, y=median, color=gen_time)) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==4 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=gen_time), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==4 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=gen_time), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_gentime |> filter(timepoint==5 | is.na(timepoint)), mapping=aes(x=date, y=median, color=gen_time)) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==5 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=gen_time), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_gentime |> filter(timepoint==5 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=gen_time), alpha=0.2) +
+## Generation time plot ##
 
-scale_fill_viridis(discrete=TRUE, name="Generation time") +
+# Simulated data
+forecasts_gentime <- ggplot() + geom_line(sim_data_cases, mapping=aes(x=date, y=value))
+# Forecasts
+for(i in 1:max(res_samples_plot_gentime$timepoint)){
+  forecasts_gentime <- forecasts_gentime + 
+    geom_line(res_samples_plot_gentime |> filter(timepoint==i | is.na(timepoint)), mapping=aes(x=date, y=median, color=gen_time)) +
+    geom_ribbon(res_samples_plot_gentime |> filter(timepoint==i | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=gen_time), alpha=0.5) +
+    geom_ribbon(res_samples_plot_gentime |> filter(timepoint==i | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=gen_time), alpha=0.2)
+}
+# Aesthetics
+forecasts_gentime <- forecasts_gentime + 
+  scale_fill_viridis(discrete=TRUE, name="Generation time") +
   scale_colour_viridis(discrete=TRUE, name="Generation time") +
   xlab("Date") +
-  ylab("Infections") +
+  ylab("Reported casts") +
   facet_wrap(~gen_time, ncol=1) +
   lshtm_theme() +
-  ylim(0, max(ebola_samples_plot_gentime$upper90))
+  ylim(0, max(res_samples_plot_gentime$upper90))
 
 #### Incubation period plot ####
 
-ebola_samples_plot_incperiod <- ebola_samples_plot |>
+res_samples_plot_incperiod <- res_samples_plot |>
   filter(is.na(gen_time) | gen_time=="correct")
 
-ebola_samples_plot_incperiod <- sample_to_quantile(ebola_samples_plot_incperiod, quantiles = c(0.05, 0.25, 0.5, 0.75, 0.95), type = 7)
+res_samples_plot_incperiod <- sample_to_quantile(res_samples_plot_incperiod, quantiles = c(0.05, 0.25, 0.5, 0.75, 0.95), type = 7)
 
-ebola_samples_plot_incperiod <- ebola_samples_plot_incperiod |>
+res_samples_plot_incperiod <- res_samples_plot_incperiod |>
   pivot_wider(names_from="quantile",
               values_from="prediction")
 
-ebola_samples_plot_incperiod <- ebola_samples_plot_incperiod |>
+res_samples_plot_incperiod <- res_samples_plot_incperiod |>
   rename(lower90="0.05",
          upper90="0.95",
          lower50="0.25",
          upper50="0.75",
-         median="0.5") # |>
-# filter(!is.na(gen_time))
+         median="0.5") 
 
 # Get rid of NA
-ebola_samples_plot_incperiod <- ebola_samples_plot_incperiod |>
+res_samples_plot_incperiod <- res_samples_plot_incperiod |>
   filter(!is.na(inc_period))
 
-plot_incperiod <- ggplot() +
-  geom_line(ebola_sim_data_infections, mapping=aes(x=date, y=value)) +
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==1 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==1 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==1 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==2 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==2 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==2 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==3 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==3 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==3 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==4 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==4 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==4 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==5 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==5 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==5 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==6 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==6 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==6 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==7 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==7 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==7 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) +
-  
-  geom_line(ebola_samples_plot_incperiod |> filter(timepoint==8 | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==8 | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
-  geom_ribbon(ebola_samples_plot_incperiod |> filter(timepoint==8 | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2) + 
-  
+## Incubation period plot ##
+
+# Simulated data
+forecasts_incperiod <- ggplot() + geom_line(sim_data_cases, mapping=aes(x=date, y=value))
+# Forecasts
+for(i in 1:max(res_samples_plot_incperiod$timepoint)){
+  forecasts_incperiod <- forecasts_incperiod + 
+    geom_line(res_samples_plot_incperiod |> filter(timepoint==i | is.na(timepoint)), mapping=aes(x=date, y=median, color=inc_period)) +
+    geom_ribbon(res_samples_plot_incperiod |> filter(timepoint==i | is.na(timepoint)), mapping=aes(x=date, ymin=lower50, ymax=upper50, fill=inc_period), alpha=0.5) +
+    geom_ribbon(res_samples_plot_incperiod |> filter(timepoint==i | is.na(timepoint)), mapping=aes(x=date, ymin=lower90, ymax=upper90, fill=inc_period), alpha=0.2)
+}
+# Aesthetics
+forecasts_incperiod <- forecasts_incperiod + 
   scale_fill_viridis(discrete=TRUE, name="Incubation period") +
   scale_colour_viridis(discrete=TRUE, name="Incubation period") +
   xlab("Date") +
-  ylab("Infections") +
-  #facet_wrap(~inc_period, ncol=1) +
+  ylab("Reported casts") +
+  facet_wrap(~inc_period, ncol=1) +
   lshtm_theme() +
-  ylim(0, max(ebola_samples_plot_incperiod$upper50))
+  ylim(0, max(res_samples_plot_incperiod$upper90))
 
-plot_grid(plot_gentime, plot_incperiod, ncol=2)
+
+plot_grid(forecasts_gentime, forecasts_incperiod, ncol=2)
+          }
+
 
