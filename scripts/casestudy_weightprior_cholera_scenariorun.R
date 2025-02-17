a2a97a4f491d6a02221aba8c02ae4a025625495e
ciaramccarthy1 <ciara.mccarthy1@lshtm.ac.uk>
2025-02-17 15:53:38 +0000

    case study plots

--- scripts/casestudy_weightprior_cholera_scenariorun.R
+++ scripts/casestudy_weightprior_cholera_scenariorun.R
@@ -38,7 +38,7 @@ cholera_rep_params <- get_parameters(fix_dist(cholera_reporting_delay))
 
 ############### SCENARIOS #################
 
-## Run scenario 25 - rt_opts=latest, under-reporting=no, weight_prior=FALSE ##
+## Run scenario 25 - rt_opts=latest, under-reporting=no, weight_prior=TRUE ##
 
 res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
                            gt,
@@ -54,16 +54,16 @@ res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
                            freq_fc=4,
                            weeks_inc=12,
                            rt_opts_choice="latest",
+                           weight_prior=TRUE,
                            obs_scale=1)
 
-save_latest(res_cholera[[2]], here("results"), paste0("res_cholerascen25_id", gt))
-save_latest(res_cholera[[3]], here("results"), paste0("res_cholerascen25_warnings", gt))
+save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen25_samples", gt))
+save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen25_id", gt))
+save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen25_R", gt))
+save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen25_summary", gt))
+save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen25_warnings", gt))
 
-## Saving samples only ##
-save_latest(res_cholera[[1]], here("results"), paste0("res_cholerascen25_samples", gt))
-save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen25_R", gt)) 
-
-## Run scenario 26 - rt_opts=latest, under-reporting=yes, weight_prior=FALSE ##
+## Run scenario 26 - rt_opts=latest, under-reporting=yes, weight_prior=TRUE ##
 
   res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
                            gt,
@@ -79,16 +79,16 @@ save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen25_R", gt)
                            freq_fc=4,
                            weeks_inc=12,
                            rt_opts_choice="latest",
+                           weight_prior=TRUE,
                            obs_scale=0.28)
 
-save_latest(res_cholera[[2]], here("results"), paste0("res_cholerascen26_id", gt))
-save_latest(res_cholera[[3]], here("results"), paste0("res_cholerascen26_warnings", gt))
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen26_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen26_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen26_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen26_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen26_warnings", gt))
 
-## Saving samples only ##
-save_latest(res_cholera[[1]], here("results"), paste0("res_cholerascen26_samples", gt))
-save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen26_R", gt)) 
-
-## Run scenario 27 - rt_opts=project, under-reporting=no, weight_prior=FALSE ##
+## Run scenario 27 - rt_opts=project, under-reporting=no, weight_prior=TRUE ##
 
   res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
                              gt,
@@ -104,17 +104,17 @@ save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen26_R", gt)
                              freq_fc=4,
                              weeks_inc=12,
                              rt_opts_choice="project",
+                             weight_prior=TRUE,
                              obs_scale=1)
   
   
-  save_latest(res_cholera[[2]], here("results"), paste0("res_cholerascen27_id", gt))
-  save_latest(res_cholera[[3]], here("results"), paste0("res_cholerascen27_warnings", gt))
-  
-  ## Saving samples only ##
-  save_latest(res_cholera[[1]], here("results"), paste0("res_cholerascen27_samples", gt))
-  save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen27_R", gt)) 
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen27_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen27_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen27_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen27_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen27_warnings", gt))
 
-## Run scenario 28 - rt_opts=project, under-reporting=yes, weight_prior=FALSE ##
+## Run scenario 28 - rt_opts=project, under-reporting=yes, weight_prior=TRUE ##
 
   res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
                              gt,
@@ -130,12 +130,114 @@ save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen26_R", gt)
                              freq_fc=4,
                              weeks_inc=12,
                              rt_opts_choice="project",
+                             weight_prior=TRUE,
                              obs_scale=0.28)
   
-  save_latest(res_cholera[[2]], here("results"), paste0("res_cholerascen28_id", gt))
-  save_latest(res_cholera[[3]], here("results"), paste0("res_cholerascen28_warnings", gt))
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen28_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen28_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen28_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen28_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen28_warnings", gt))
   
-  ## Saving samples only ##
-  save_latest(res_cholera[[1]], here("results"), paste0("res_cholerascen28_samples", gt))
-  save_latest(res_cholera[[4]], here("results"), paste0("res_cholerascen28_R", gt)) 
+  ## Run scenario 29 - rt_opts=latest, under-reporting=no, weight_prior=FALSE ##
+  
+  res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
+                                 gt,
+                                 gen_mean=5,
+                                 gen_sd=8, 
+                                 gen_max=30,
+                                 inc_mean=1.4,
+                                 inc_sd=1.98, 
+                                 inc_max=30,
+                                 rep_meanlog=cholera_rep_params$meanlog,
+                                 rep_sdlog=cholera_rep_params$sdlog,
+                                 rep_max=30,
+                                 freq_fc=4,
+                                 weeks_inc=12,
+                                 rt_opts_choice="latest",
+                                 weight_prior=FALSE,
+                                 obs_scale=1)
+  
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen29_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen29_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen29_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen29_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen29_warnings", gt))
+  
+  ## Run scenario 30 - rt_opts=latest, under-reporting=yes, weight_prior=FALSE ##
+  
+  res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
+                                 gt,
+                                 gen_mean=5,
+                                 gen_sd=8, 
+                                 gen_max=30,
+                                 inc_mean=1.4,
+                                 inc_sd=1.98, 
+                                 inc_max=30,
+                                 rep_meanlog=cholera_rep_params$meanlog,
+                                 rep_sdlog=cholera_rep_params$sdlog,
+                                 rep_max=30,
+                                 freq_fc=4,
+                                 weeks_inc=12,
+                                 rt_opts_choice="latest",
+                                 weight_prior=FALSE,
+                                 obs_scale=0.28)
+  
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen30_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen30_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen30_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen30_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen30_warnings", gt))
+  
+  ## Run scenario 31 - rt_opts=project, under-reporting=no, weight_prior=FALSE ##
+  
+  res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
+                                 gt,
+                                 gen_mean=5,
+                                 gen_sd=8, 
+                                 gen_max=30,
+                                 inc_mean=1.4,
+                                 inc_sd=1.98, 
+                                 inc_max=30,
+                                 rep_meanlog=cholera_rep_params$meanlog,
+                                 rep_sdlog=cholera_rep_params$sdlog,
+                                 rep_max=30,
+                                 freq_fc=4,
+                                 weeks_inc=12,
+                                 rt_opts_choice="project",
+                                 weight_prior=FALSE,
+                                 obs_scale=1)
+  
+  
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen31_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen31_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen31_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen31_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen31_warnings", gt))
+  
+  ## Run scenario 32 - rt_opts=project, under-reporting=yes, weight_prior=FALSE ##
+  
+  res_cholera <- sim_weightprior(case_data=cholera_yem_tot,
+                                 gt,
+                                 gen_mean=5,
+                                 gen_sd=8, 
+                                 gen_max=30,
+                                 inc_mean=1.4,
+                                 inc_sd=1.98, 
+                                 inc_max=30,
+                                 rep_meanlog=cholera_rep_params$meanlog,
+                                 rep_sdlog=cholera_rep_params$sdlog,
+                                 rep_max=30,
+                                 freq_fc=4,
+                                 weeks_inc=12,
+                                 rt_opts_choice="project",
+                                 weight_prior=FALSE,
+                                 obs_scale=0.28)
+  
+  save_latest(res_ebola[[1]], here("results"), paste0("res_cholerascen32_samples", gt))
+  save_latest(res_ebola[[2]], here("results"), paste0("res_cholerascen32_id", gt))
+  save_latest(res_ebola[[3]], here("results"), paste0("res_cholerascen32_R", gt))
+  save_latest(res_ebola[[4]], here("results"), paste0("res_cholerascen32_summary", gt))
+  save_latest(res_ebola[[5]], here("results"), paste0("res_cholerascen32_warnings", gt))
+  
   
