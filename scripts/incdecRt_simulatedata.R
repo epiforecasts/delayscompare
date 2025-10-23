d4f5b90abf9180f509934fcc3d79ef270992a64b
ciaramccarthy1 <ciara.mccarthy1@lshtm.ac.uk>
2025-10-23 14:42:03 +0100

    making coderabbit suggested changes

--- scripts/incdecRt_simulatedata.R
+++ scripts/incdecRt_simulatedata.R
@@ -8,9 +8,9 @@ source(here("R", "scenario_loop.R"))
 disease <- "ebola"
 d <- delays[[disease]]
 
-#####################
-#### Constant Rt ####
-#####################
+######################################
+#### Increasing and decreasing Rt ####
+######################################
 
 startdate <- startenddates[[disease]][["startdate"]]
 enddate <- startenddates[[disease]][["enddate"]]
@@ -18,8 +18,8 @@ enddate <- startenddates[[disease]][["enddate"]]
 gen_time <- get(paste0(disease, "_gen_time"))
 inc_period <- get(paste0(disease, "_inc_period"))
  
-if(exists(paste0(disease, "_rep_delay"))){ # To cope with lack of Ebola reporting delay
-rep_delay <- get(paste0(disease, "_rep_delay"))
+if(exists(paste0(disease, "_reporting_delay"))){ # To cope with lack of Ebola reporting delay
+  rep_delay <- get(paste0(disease, "_reporting_delay"))
 combined_delay <- inc_period + rep_delay
 } else {combined_delay <- inc_period}
 
@@ -47,7 +47,7 @@ sim_data_inc_ur <- simulate_infections(
 save_latest(sim_data_inc, here("data"), paste0(disease, "_sim_data_inc"))
 save_latest(sim_data_inc_ur, here("data"), paste0(disease, "_sim_data_inc_ur"))
 
-##### Simulate data - high Rt ######
+##### Simulate data - decreasing Rt ######
 
 rt_dec <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
                      R=rev(seq(from=0.8, to=1.2, length.out=enddate-(startdate-1))))
@@ -68,5 +68,5 @@ sim_data_dec_ur <- simulate_infections(
   obs=obs_opts(family="poisson", scale=d$underreport)
 )
 
-save_latest(sim_data_dec, here("data"), paste0("_sim_data_dec"))
-save_latest(sim_data_dec_ur, here("data"), paste0("_sim_data_dec_ur"))
+save_latest(sim_data_dec, here("data"), paste0(disease,"_sim_data_dec"))
+save_latest(sim_data_dec_ur, here("data"), paste0(disease,"_sim_data_dec_ur"))
