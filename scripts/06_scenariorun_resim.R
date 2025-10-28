3a170bd4486d3d72d9b4481c15678bbcd521215d
ciaramccarthy1 <ciara.mccarthy1@lshtm.ac.uk>
2025-10-28 16:46:33 +0000

    tidying results processing

--- scripts/06_scenariorun_resim.R
+++ scripts/06_scenariorun_resim.R
@@ -44,31 +44,8 @@ res_disease <- sim_scenarios(case_data=sim_data_cases,
                              rt_opts_choice = rt_opts,
                              obs_scale = d$underreport)
 
-save_latest(res_disease[[1]], here("results"), paste0("res_", disease, "_sim_latest_samples", gt))
-save_latest(res_disease[[2]], here("results"), paste0("res_", disease, "_sim_latest_id", gt))
-save_latest(res_disease[[3]], here("results"), paste0("res_", disease, "_sim_latest_R", gt))
-save_latest(res_disease[[4]], here("results"), paste0("res_", disease, "_sim_latest_summary", gt))
-save_latest(res_disease[[5]], here("results"), paste0("res_", disease, "_sim_latest_warnings", gt))
-
-## Run scenario 2 - rt_opts=project, under-reporting=yes ##
-
-res_disease <- sim_scenarios(case_data=sim_data_cases,
-                             gt,
-                             gen_mean = d$gen["mean"],
-                             gen_sd = d$gen["sd"],
-                             gen_max = d$gen["max"],
-                             inc_mean = d$inc["mean"],
-                             inc_sd = d$inc["sd"],
-                             inc_max = d$inc["max"],
-                             rep_mean = d$rep["mean"],
-                             rep_sd = d$rep["sd"],
-                             rep_max = d$rep["max"],
-                             freq_fc = 4,
-                             weeks_inc = 12,
-                             rt_opts_choice = rt_opts,
-                             obs_scale = d$underreport)
-save_latest(res_disease[[1]], here("results"), paste0("res_", disease, "sim_project_samples", gt))
-save_latest(res_disease[[2]], here("results"), paste0("res_", disease, "sim_project_id", gt))
-save_latest(res_disease[[3]], here("results"), paste0("res_", disease, "sim_project_R", gt))
-save_latest(res_disease[[4]], here("results"), paste0("res_", disease, "sim_project_summary", gt))
-save_latest(res_disease[[5]], here("results"), paste0("res_", disease, "sim_project_warnings", gt))
+save_latest(res_disease[[1]], here("results"), paste0("res_", disease, "_resim_", rt_opts, "_samples", gt))
+save_latest(res_disease[[2]], here("results"), paste0("res_", disease, "_resim_", rt_opts, "_id", gt))
+save_latest(res_disease[[3]], here("results"), paste0("res_", disease, "_resim_", rt_opts, "_R", gt))
+save_latest(res_disease[[4]], here("results"), paste0("res_", disease, "_resim_", rt_opts, "_summary", gt))
+save_latest(res_disease[[5]], here("results"), paste0("res_", disease, "_resim_", rt_opts, "_warnings", gt))
