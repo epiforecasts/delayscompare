c73dddc4de24b1063c170222404259e8b30d6751
ciaramccarthy1 <ciara.mccarthy1@lshtm.ac.uk>
2026-03-04 10:33:50 +0000

    update datacollect.R

--- scripts/datacollect.R
+++ scripts/datacollect.R
@@ -1,7 +1,8 @@
+for(disease in c("covid", "ebola", "cholera")){
 
 ## Collect Rt trajectories and simulated data in a list ##
 
-disease <- "covid"
+disease <- disease
 
 ## Get correct start and end date for disease
 
@@ -10,66 +11,27 @@ enddate <- startenddates[[disease]][["enddate"]]
 
 rt_traj <- list()
 
-# Scenarios 1-4 - const Rt - low 
-for(i in 1:4){
-  rt_traj[[i]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
-                             R=0.8)}
+# Scenarios - const Rt - low 
+  rt_traj[[1]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
+                             R=0.8,
+                             scen = "const_low")
 
-# Scenarios 5-8 - const Rt - high
-for(i in 5:8){
-  rt_traj[[i]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
-                             R=1.2)}
+# Scenarios - const Rt - high
+  rt_traj[[2]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
+                             R=1.2,
+                             scen = "const_high")
 
 # Scenarios 9-12 - increasing Rt 
-for(i in 9:12){
-  rt_traj[[i]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
-                             R=seq(from=0.8, to=1.2, length.out=enddate-(startdate-1)))}
+  rt_traj[[3]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
+                             R=seq(from=0.8, to=1.2, length.out=enddate-(startdate-1)),
+                             scen = "inc")
 
 # Scenarios 13-16 - decreasing Rt
-for(i in 13:16){
-  rt_traj[[i]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
-                             R=rev(seq(from=0.8, to=1.2, length.out=enddate-(startdate-1))))}
-
-sim_data <- list()
-
-# Scenario 1 - Rt const low, rt_opts=latest, under-reporting=no
-sim_data[[1]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_low"))
-# Scenario 2 - Rt const low, rt_opts=latest, under-reporting=yes
-sim_data[[2]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_low_ur"))
-# Scenario 3 - Rt const low, rt_opts=project, ur=no
-sim_data[[3]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_low"))
-# Scenario 4 - Rt const low, rt_opts=project, ur=yes
-sim_data[[4]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_low_ur"))
-
-# Scenario 5 - Rt const high, rt_opts=latest, under-reporting=no
-sim_data[[5]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_hi"))
-# Scenario 6 - Rt const high, rt_opts=latest, under-reporting=yes
-sim_data[[6]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_hi_ur"))
-# Scenario 7 - Rt const high, rt_opts=project, under-reporting=no 
-sim_data[[7]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_hi"))
-# Scenario 8 - Rt const high, rt_opts=project, under-reporting=yes
-sim_data[[8]] <- read_latest(here("data"), paste0(disease, "_sim_data_const_hi_ur"))
-
-# Scenario 9 - Rt inc, rt_opts=latest, under-reporting=no
-sim_data[[9]] <- read_latest(here("data"), paste0(disease, "_sim_data_inc"))
-# Scenario 10 - Rt inc, rt_opts=latest, under-reporting=yes
-sim_data[[10]] <- read_latest(here("data"), paste0(disease, "_sim_data_inc_ur"))
-# Scenario 11 - Rt inc, rt_opts=project, under-reporting=no
-sim_data[[11]] <- read_latest(here("data"), paste0(disease, "_sim_data_inc"))
-# Scenario 12 - Rt inc, rt_opts=project, under-reporting=yes
-sim_data[[12]] <- read_latest(here("data"), paste0(disease, "_sim_data_inc_ur"))
-
-# Scenario 13 - Rt dec, rt_opts=latest, under-reporting=no
-sim_data[[13]] <- read_latest(here("data"), paste0(disease, "_sim_data_dec"))
-# Scenario 14 - Rt dec, rt_opts=latest, under-reporting=yes
-sim_data[[14]] <- read_latest(here("data"), paste0(disease, "_sim_data_dec_ur"))
-# Scenario 15 - Rt dec, rt_opts=project, under-reporting=no 
-sim_data[[15]] <- read_latest(here("data"), paste0(disease, "_sim_data_dec"))
-# Scenario 16 - Rt dec, rt_opts=project, under-reporting=yes
-sim_data[[16]] <- read_latest(here("data"), paste0(disease, "_sim_data_dec_ur"))
+  rt_traj[[4]] <- data.frame(date=seq.Date(from=startdate, to=enddate, by=1),
+                             R=rev(seq(from=0.8, to=1.2, length.out=enddate-(startdate-1))),
+                             scen = "dec")
 
 ## Save ##
 
 save_latest(rt_traj, here("data"), paste0("rt_traj_list_", disease))
-save_latest(sim_data, here("data"), paste0("sim_data_list_", disease))
-
+}
